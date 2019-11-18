5d2e06dc9463863325c3d30b1054f44bd13ec694
Joseph Herradi <joseph.herradi@gmail.com>
2019-11-18 18:37:49 +0100

    impl reservation queue list limit

--- webservices/src/main/java/com/ocp7/webservices/Controller/ReservationController.java
+++ webservices/src/main/java/com/ocp7/webservices/Controller/ReservationController.java
@@ -1,5 +1,8 @@
 package com.ocp7.webservices.Controller;
 
+import com.ocp7.webservices.Controller.exceptions.ImpossibleAjouterReservationException;
+import com.ocp7.webservices.DAO.LivreDAO;
+import com.ocp7.webservices.DAO.ReservationDAO;
 import com.ocp7.webservices.Modele.Livre;
 import com.ocp7.webservices.Modele.Reservation;
 import com.ocp7.webservices.Service.LivreService;
@@ -11,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ReservationController {
@@ -18,17 +22,30 @@ public class ReservationController {
     @Autowired
     private ReservationService reservationService;
 
+    @Autowired
+    private LivreDAO livreDAO;
+
+    @Autowired
+    private ReservationDAO reservationDAO;
+
 
     @RequestMapping(value = "livre/{livreId}/reservation/showFormResa", method = RequestMethod.GET)
-    public Reservation showFormForAdd(@PathVariable("livreId") int pretId, Model model){
-        Reservation laReservation= new Reservation();
+    public Reservation showFormForAdd(@PathVariable("livreId") int pretId, Model model) {
+        Reservation laReservation = new Reservation();
         return laReservation;
     }
 
-    @RequestMapping(value = "livre/{livreId}/reservation/saveFormResa",method =RequestMethod.POST)
-    public ResponseEntity<Reservation> saveReservation(@PathVariable("livreId") int livreId, @RequestBody Reservation laReservation){
-        Reservation lareservation=reservationService.saveReservation(livreId,laReservation);
-        return new ResponseEntity<Reservation>(laReservation, HttpStatus.CREATED);
+    @RequestMapping(value = "livre/{livreId}/reservation/saveFormResa", method = RequestMethod.POST)
+    public ResponseEntity<Reservation> saveReservation(@PathVariable("livreId") int livreId, @RequestBody Reservation laReservation) {
+        List<Reservation> resasBook = reservationDAO.findByLivre(livreDAO.findById(livreId).orElse(null).getNom()).orElse(null);
+
+        if (resasBook==null||(resasBook.size()+1) <= 2 * livreDAO.findById(livreId).orElse(null).getQuantite()) {
+
+            Reservation lareservation = reservationService.saveReservation(livreId, laReservation);
+            return new ResponseEntity<Reservation>(laReservation, HttpStatus.CREATED);
+        } else
+            throw new ImpossibleAjouterReservationException("Liste d'attente complète, impossible de réserver pour l'instant");
+
     }
 
     @RequestMapping(value = "userReservations",method=RequestMethod.GET)
