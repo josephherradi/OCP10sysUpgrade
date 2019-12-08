ac421faf08ec0f338ed86f42ccce2b2e1a980dde
Joseph Herradi <joseph.herradi@gmail.com>
2019-12-08 16:35:04 +0100

    test context initialization

--- webservices/src/test/java/com/oc/p7/api/pret/ApplicationTests.java
+++ webservices/src/test/java/com/oc/p7/api/pret/ApplicationTests.java
@@ -1,13 +1,31 @@
 package com.oc.p7.api.pret;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.ocp7.webservices.Application;
+import com.ocp7.webservices.DAO.LivreDAO;
+import com.ocp7.webservices.Modele.Livre;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+
+@SpringBootTest(classes={Application.class})
+@ExtendWith(SpringExtension.class)
+
 
 public class ApplicationTests {
+	@Autowired
+	private LivreDAO livreDAO;
 
-	public void contextLoads() {
+	@Test
+	public void checkdDAO() {
+		List<Livre> livreList=livreDAO.findAll();
+		Assert.isTrue(livreList.size()==5,"error nombre livre erroné");
 	}
 
 }
