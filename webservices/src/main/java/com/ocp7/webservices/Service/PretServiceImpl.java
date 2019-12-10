9593da6bb8395c6271bc1c3a20fe600c6afae669
Joseph Herradi <joseph.herradi@gmail.com>
2019-12-10 12:00:58 +0100

    some test fixes

--- webservices/src/main/java/com/ocp7/webservices/Service/PretServiceImpl.java
+++ webservices/src/main/java/com/ocp7/webservices/Service/PretServiceImpl.java
@@ -110,5 +110,4 @@ public class PretServiceImpl implements PretService {
         return pretRetourPlusproche;
     }
 
-
 }
--- webservices/src/test/java/com/oc/p7/api/pret/ApplicationTests.java
+++ webservices/src/test/java/com/oc/p7/api/pret/ApplicationTests.java
@@ -1,9 +1,14 @@
 package com.oc.p7.api.pret;
 
 import com.ocp7.webservices.Application;
+import com.ocp7.webservices.Controller.exceptions.FunctionalException;
 import com.ocp7.webservices.DAO.LivreDAO;
 import com.ocp7.webservices.Modele.Livre;
 
+import com.ocp7.webservices.Modele.Pret;
+import com.ocp7.webservices.Service.PretService;
+import com.ocp7.webservices.Service.PretServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.util.Assert;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -20,12 +26,48 @@ import java.util.List;
 
 public class ApplicationTests {
 	@Autowired
-	private LivreDAO livreDAO;
+	private PretServiceImpl pretServiceimpl;
 
 	@Test
-	public void checkdDAO() {
-		List<Livre> livreList=livreDAO.findAll();
-		Assert.isTrue(livreList.size()==5,"error nombre livre erroné");
+	public void checkBookDispo1() throws FunctionalException {
+		Pret lePret= new Pret();
+		lePret.setIdLivre(17);
+		lePret.setTagForUpdate(Boolean.FALSE);
+		Assertions.assertThrows(FunctionalException.class,(() -> {
+			pretServiceimpl.checkDispoLivre(lePret);
+		}));
 	}
+
+	@Test
+	public void checkBookDispo2() throws FunctionalException {
+        Pret lePret = new Pret();
+        lePret.setIdLivre(1);
+        lePret.setTagForUpdate(Boolean.FALSE);
+        Assertions.assertDoesNotThrow(() -> {
+            pretServiceimpl.checkDispoLivre(lePret);
+        });
+    }
+
+	@Test
+	public void nouveauPret(){
+		Pret lePret=new Pret();
+		Livre leLivre=new Livre();
+		leLivre.setDisponibilite(4);
+		pretServiceimpl.nouveauPretProcessing(lePret,leLivre);
+		Assert.isTrue(leLivre.getDisponibilite()==3,"erreur maj disponibilite");
+	}
+
+	@Test
+	public void retourPret(){
+		Pret lePret=new Pret();
+		Livre leLivre=new Livre();
+		leLivre.setDisponibilite(4);
+		pretServiceimpl.retourPretProcessing(lePret,leLivre);
+		Assert.isTrue(leLivre.getDisponibilite()==5,"erreur maj disponibilite");
+	}
+
+
+
+
 
 }
